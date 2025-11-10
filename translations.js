c931c00cc835c85e37bb7528715530d40ac4d77e
Josh <dev@cldv.org>
2025-11-10 21:55:30 +0100

    add translations

--- translations.js
+++ translations.js
@@ -7,6 +7,174 @@ const translationData = {
     {
       "ngeakish": ["ngeak off"],
       "english": "Going offline"
+    },
+    {
+      "ngeakish": ["Ngeak off"],
+      "english": "Turn off"
+    },
+    {
+      "ngeakish": ["ngeak you"],
+      "english": "Fuck you"
+    },
+    {
+      "ngeakish": ["ngeak me"],
+      "english": "Fuck me"
+    },
+    {
+      "ngeakish": ["Ngeal ne", "ngwak ngek ngeak", "ngenge ngeak", "Nagayk"],
+      "english": "How are you"
+    },
+    {
+      "ngeakish": ["ngeal ne", "ngeak ngi ngi"],
+      "english": "I don't know"
+    },
+    {
+      "ngeakish": ["ngeao! ngeal ne"],
+      "english": "Hey! What's up"
+    },
+    {
+      "ngeakish": ["Ngeao! Ngeal ne"],
+      "english": "Hello! What's up"
+    },
+    {
+      "ngeakish": ["ngeeak ngi gugu ngaa ngek"],
+      "english": "Why are you laughing"
+    },
+    {
+      "ngeakish": ["ngek ngoo ngungu ngeak"],
+      "english": "What are you doing"
+    },
+    {
+      "ngeakish": ["ass ngeak", "ngi ngi ngeak ngu", "ngayk"],
+      "english": "What's up"
+    },
+    {
+      "ngeakish": ["ngeak ass"],
+      "english": "Neat ass"
+    },
+    {
+      "ngeakish": ["nggeak nggak"],
+      "english": "No snoring"
+    },
+    {
+      "ngeakish": ["nggak ngeak?"],
+      "english": "Not laughing?"
+    },
+    {
+      "ngeakish": ["nggak hole nggeak"],
+      "english": "There is no hole"
+    },
+    {
+      "ngeakish": ["nggak ass"],
+      "english": "Not ass"
+    },
+    {
+      "ngeakish": ["ngengeak ya"],
+      "english": "Are you kidding"
+    },
+    {
+      "ngeakish": ["ngak nggeak"],
+      "english": "It's not possible"
+    },
+    {
+      "ngeakish": ["gak ngeak ngi ngeak"],
+      "english": "I don't care"
+    },
+    {
+      "ngeakish": ["gak ngi ngi geak"],
+      "english": "I don't know, dude"
+    },
+    {
+      "ngeakish": ["ngek ngeak asa"],
+      "english": "It's scary"
+    },
+    {
+      "ngeakish": ["ass ngek ngi ngeak"],
+      "english": "My stomach hurts"
+    },
+    {
+      "ngeakish": ["ngi ngeak ngakak nge ngik ngaek"],
+      "english": "Caught laughing and giggling"
+    },
+    {
+      "ngeakish": ["ngu ngegue ngeak nge"],
+      "english": "Are you kidding or not"
+    },
+    {
+      "ngeakish": ["ngiek ngeke ngi"],
+      "english": "It's annoying"
+    },
+    {
+      "ngeakish": ["ngekke ngik ngaek"],
+      "english": "Don't complain"
+    },
+    {
+      "ngeakish": ["ngi ngje neghe nghei"],
+      "english": "I won't go anywhere"
+    },
+    {
+      "ngeakish": ["negeh ngeak ngi ngek"],
+      "english": "Don't worry about it"
+    },
+    {
+      "ngeakish": ["ngini ngek"],
+      "english": "This sucks"
+    },
+    {
+      "ngeakish": ["ngiwnguwngene ngekngeki"],
+      "english": "I'm really tired"
+    },
+    {
+      "ngeakish": ["ngenge ngu"],
+      "english": "You're stupid"
+    },
+    {
+      "ngeakish": ["Ngeak derei"],
+      "english": "Goodbye"
+    },
+    {
+      "ngeakish": ["enak ngeak nggak"],
+      "english": "Is it enjoyable to eat"
+    },
+    {
+      "ngeakish": ["Ngeak negnak no"],
+      "english": "No, I don't want to"
+    },
+    {
+      "ngeakish": ["ngeak negnak no"],
+      "english": "I don't want to talk anymore"
+    },
+    {
+      "ngeakish": ["ngi ngeak ngake ngi nga ngahu"],
+      "english": "Crying and laughing and laughing"
+    },
+    {
+      "ngeakish": ["ngini nggak ngek"],
+      "english": "This isn't funny"
+    },
+    {
+      "ngeakish": ["ngeak pee"],
+      "english": "Fuck it"
+    },
+    {
+      "ngeakish": ["ngeak ni ngu pee ngon ngik ngi"],
+      "english": "I'm having a hard time"
+    },
+    {
+      "ngeakish": ["Nagakak"],
+      "english": "I'm awake"
+    },
+    {
+      "ngeakish": ["Ngaek"],
+      "english": "No way"
+    },
+    {
+      "ngeakish": ["ngesk"],
+      "english": "Thank you"
+    },
+    {
+      "ngeakish": ["nagayk"],
+      "english": "I'm tired"
     }
   ],
   "words": [
@@ -17,6 +185,42 @@ const translationData = {
     {
       "ngeakish": ["Ngeak"],
       "english": "Naughty"
+    },
+    {
+      "ngeakish": ["nagakak"],
+      "english": "Naked"
+    },
+    {
+      "ngeakish": ["ngaek"],
+      "english": "Damn"
+    },
+    {
+      "ngeakish": ["ngrak"],
+      "english": "Angry"
+    },
+    {
+      "ngeakish": ["Ngrak"],
+      "english": "Scared"
+    },
+    {
+      "ngeakish": ["Ngrak"],
+      "english": "Scared"
+    },
+    {
+      "ngeakish": ["ngeao"],
+      "english": "Hey"
+    },
+    {
+      "ngeakish": ["ngeakish"],
+      "english": "Ignoring"
+    },
+    {
+      "ngeakish": ["Ngeakish"],
+      "english": "Naughty"
+    },
+    {
+      "ngeakish": ["nggak"],
+      "english": "No"
     }
   ]
 };
